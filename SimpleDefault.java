7b3d28f4e01c3f5d4f01695e3fb9732fa9a82b5e
Ebin008 <76677091+Ebin008@users.noreply.github.com>
2021-05-25 19:15:18 +0530

    Add files via upload

---
+++ SimpleDefault.java
@@ -0,0 +1,18 @@
+class A
+{
+int data=40;
+void msg()
+{
+System.out.println("Hello java");
+}
+}
+public class SimpleDefault
+{
+public static void main(String a[])
+{
+ A obj=new A();
+ System.out.println(obj.data);
+ obj.msg();
+}
+}
+
