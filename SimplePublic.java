55d3133ddc94cd550f1bf34a147b78180d491e67
Ebin008 <76677091+Ebin008@users.noreply.github.com>
2021-05-25 19:13:30 +0530

    Add files via upload

---
+++ SimplePublic.java
@@ -0,0 +1,18 @@
+class A
+{
+public int data=40;
+public void msg()
+{
+System.out.println("Hello java");
+}
+}
+public class SimplePublic
+{
+public static void main(String a[])
+{
+A obj=new A();
+System.out.println(obj.data);
+obj.msg();
+}
+}
+
